bfd99b3b9f801ad5312b8c9f419f7977855a661c
kitdea <francismarrosales@gmail.com>
2021-08-19 21:54:08 +0800

    add footer markup

---
+++ template-parts/dynamic-footer.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The template for displaying footer.
+ *
+ * @package HelloElementor
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+?>
+<footer id="site-footer" class="em-footer--container" role="contentinfo">
+	<div class="em-footer--inner">
+		<h4>This is footer!</h4>
+	</div>
+</footer> <!-- .em-footer--container -->
